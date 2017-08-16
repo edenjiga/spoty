8e967454fe6f286e48e7fb55c850f26a3be2b6dc
Edgar Enrique Jimenez Garcia <edenjiga_@hotmail.com>
2017-08-16 08:20:20 -0500

    Falta hacer el reproductor y la persistencia de este

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,7 +12,11 @@ import {routing, appRoutingProviders} from './app.routing';
 import { AppComponent } from './app.component';
 
 import { HomeComponent } from './components/home.component';
+
+// user
 import {UserEditComponent} from './components/user-edit.component';
+
+// Artista
 import {ArtistListComponent} from './components/artist-list.component';
 import {ArtistAddComponent} from './components/artist-add.component';
 import {ArtistEditComponent} from './components/artist-edit.component';
@@ -22,6 +26,7 @@ import {AlbumEditComponent} from './components/album-edit.component';
 import {AlbumDetailComponent} from './components/album-detail.component';
 import {SongAddComponent} from './components/song-add.component';
 import {SongEditComponent} from './components/song-edit.component';
+import {PlayerComponent} from './components/player.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +41,8 @@ import {SongEditComponent} from './components/song-edit.component';
     AlbumEditComponent,
     AlbumDetailComponent,
     SongAddComponent,
-    SongEditComponent
+    SongEditComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/player.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit} from '@angular/core';
+import {Song} from '../models/song';
+import {GLOBAL} from '../services/global';
+
+@Component({
+    selector: 'player',
+    templateUrl: '../views/player.html'
+})
+
+export class PlayerComponent implements OnInit{
+
+    public url: string;
+    public song: Song;
+
+    constructor(){
+        this.url = GLOBAL.url;
+        this.song = new Song(1, '', '', '', '');
+    }
+    ngOnInit(){
+        console.log('player cargado');
+    }
+}
